e4cb380356a6b831a06d8b404b4ab6bf7eadb0fd
Kraimps Pierre <pier_lupo@laposte.net>
2023-07-15 14:38:16 +0200

    still wip

--- src/main/java/com/example/SpringTodo/entity/Image.java
+++ src/main/java/com/example/SpringTodo/entity/Image.java
@@ -1,10 +1,16 @@
 package com.example.SpringTodo.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Image {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
